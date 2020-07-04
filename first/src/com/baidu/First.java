9dbd0b69b0840ec0e7bc89f65b7cdacdfc77db95
ghity <standingby_2019@qq.com>
2020-07-04 10:22:14 +0800

    我的第一个项目

---
+++ first/src/com/baidu/First.java
@@ -0,0 +1,17 @@
+package com.baidu;
+
+import java.util.Scanner;
+
+/**
+ * @Author: ghity
+ * @Date: 2020/7/4 9:52
+ * com.baidu
+ * first
+ */
+public class First {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int i = sc.nextInt();
+        System.out.println(i);
+    }
+}
